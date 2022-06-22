6cccc5b2b96cdb7e0262a3547ef8c44ca22da1cf
AB59574 <AB59574@POOLW42NLPC1094.mshome.net>
2022-06-22 11:28:44 +0530

    external properties file for configuration of environments.

--- src/main/java/com/abhiroop/spring/springin10steps/SpringIn10StepsXmlContextApplication.java
+++ src/main/java/com/abhiroop/spring/springin10steps/SpringIn10StepsXmlContextApplication.java
@@ -1,17 +1,18 @@
 package com.abhiroop.spring.springin10steps;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.abhiroop.spring.springin10steps.xml.XMLPersonDAO;
 
 public class SpringIn10StepsXmlContextApplication {
-
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn10StepsXmlContextApplication.class);
 	public static void main(String[] args) {
 		try(ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("ApplicationContext.xml")) {
+			LOGGER.info("Beans Loaded -> {}", (Object)applicationContext.getBeanDefinitionNames());
 		XMLPersonDAO personDAO =applicationContext.getBean(XMLPersonDAO.class);
-		System.out.println(personDAO);
-		System.out.println(personDAO.getJdbcConnection());
+		LOGGER.info("{}", personDAO);
+		LOGGER.info("jdbc - {}",personDAO.getJdbcConnection());
 	}
 	}
 
